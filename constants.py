5eef52f8e8b920ea3ca169ce88a43bdb6b7cf06c
GauravTiwari07 <tgaurav72@gmail.com>
2017-07-07 18:44:19 +0530

    my first commmit

---
+++ constants.py
@@ -0,0 +1,6 @@
+#all global variable is here.
+
+#instagram api access token
+APP_ACCESS_TOKEN = '1828417204.5af35d8.2e88b9ddb30f4dbc8c49fb1182eed911'
+#instagram api base url
+BASE_URL = 'https://api.instagram.com/v1/'
