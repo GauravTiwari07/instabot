1b7b2e4b2a6efd29d312a48c475c9394be091e89
GauravTiwari07 <tgaurav72@gmail.com>
2017-07-09 21:32:32 +0530

    added comment _on_posts

---
+++ comment_on _post.py
@@ -0,0 +1,16 @@
+import requests
+import urllib
+from constants import BASE_URL, APP_ACCESS_TOKEN
+from get_user_posts import get_users_post
+def comment_on_a_users_post(insta_username):
+    post_id = get_users_post(insta_username)
+    print("post_id = ", post_id)
+    request_url = BASE_URL + "media/{}/comments".format(post_id)
+    print("Requesting:\n{}".format(request_url))
+    comment_to_post = input("Enter your comment: ")
+    comment = requests.post(request_url, data={'access_token': APP_ACCESS_TOKEN, 'text': comment_to_post}).json()
+    print(comment)
+    if comment['meta']['code'] == 200:
+        print("comment was successful")
+    else:
+        print("comment not successful")
--- list_comments.py
+++ list_comments.py
@@ -3,7 +3,7 @@ from constants import BASE_URL, APP_ACCESS_TOKEN
 from get_user_posts import get_users_post
 
 def get_list_of_comments_on_users_post(insta_username):
-    post_id =  get_user_posts(insta_username)
+    post_id =  get_users_post(insta_username)
     print("post_id = ", post_id)
     request_url = BASE_URL + "media/{}/comments?access_token={}".format(post_id, APP_ACCESS_TOKEN)
     print("Requesting:\n{}".format(request_url))
